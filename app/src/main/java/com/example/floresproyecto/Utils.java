2d33875152e468699c72d92095344db212d7455e
BrandonBreack <brandon_enrique17@hotmail.com>
2021-12-12 23:02:47 -0600

    Update Utils.java
    nada

--- app/src/main/java/com/example/floresproyecto/Utils.java
+++ app/src/main/java/com/example/floresproyecto/Utils.java
@@ -3,5 +3,4 @@ package com.example.floresproyecto;
 public class Utils {
     public static final String EMAIL = "TiendaDeFloresProyecto@gmail.com";
     public static final String PASSWORD = "tkyfvokjpggpurnq";
-
 }
