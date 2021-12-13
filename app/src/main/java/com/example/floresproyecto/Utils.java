3f2b3b55e6eb03b1000271cd7df9b469a026d2a8
BrandonBreack <brandon_enrique17@hotmail.com>
2021-12-12 22:57:19 -0600

    Contraseña

--- app/src/main/java/com/example/floresproyecto/Utils.java
+++ app/src/main/java/com/example/floresproyecto/Utils.java
@@ -2,6 +2,6 @@ package com.example.floresproyecto;
 
 public class Utils {
     public static final String EMAIL = "TiendaDeFloresProyecto@gmail.com";
-    public static final String PASSWORD = "tqaqewqhghrzomwi";
+    public static final String PASSWORD = "tkyfvokjpggpurnq";
 
 }
